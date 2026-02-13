aac93ff907fa933f9cb9cbe5051d4a237df032e4
sjsuriya2528 <jayasuriyas.cse2024@citchennai.net>
2026-02-13 14:16:06 +0530

    deploy code

--- campa-distributor-system/backend/models/index.js
+++ campa-distributor-system/backend/models/index.js
@@ -6,6 +6,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: process.env.NODE_ENV === 'production' ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    } : {}
 });
 
 const db = {};
