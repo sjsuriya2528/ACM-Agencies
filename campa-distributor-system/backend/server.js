212e3b9bb8c9598639f562309cfae1be8ef2df13
sjsuriya2528 <jayasuriyas.cse2024@citchennai.net>
2026-02-22 21:54:15 +0530

    final push

--- campa-distributor-system/backend/server.js
+++ campa-distributor-system/backend/server.js
@@ -23,7 +23,9 @@ const corsOptions = {
     'http://localhost:5174',
     'http://localhost:5175',
     'http://localhost:5176',
-    'http://localhost:5177'
+    'http://localhost:5177',
+    'https://acm-agencies-admin.vercel.app',
+    'https://acm-agencies-employee.vercel.app'
   ],
   optionsSuccessStatus: 200,
 };
